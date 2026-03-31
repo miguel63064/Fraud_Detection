d173cbbf6b28784e81636b00732e7cf6f314df4a
Miguel <miguel.r.ferreira@tecnico.ulisboa.pt>
2026-03-31 18:32:06 +0100

    Docker-compose and more test

--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def submission(test_ids, test_preds):
     submission = pd.DataFrame({"TransactionID": test_ids, "isFraud": test_preds})
     submission.to_csv("submissions/submission.csv", index=False)
